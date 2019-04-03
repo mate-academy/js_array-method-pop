807d07aad4869edbc11202552d7d0ddd2b3eceb5
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-03 12:50:28 +0300

    initial commit

---
+++ src/arrayMethodLastIndexOf.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * Implement method lastIndexOf
+ *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/lastIndexOf
+ */
+
+function applyCustomLastIndexOf() {
+  [].__proto__.lastIndexOf2 = function() {
+    // write code here
+  };
+}
+
+module.exports = applyCustomLastIndexOf;
