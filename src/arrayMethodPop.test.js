d284c41ae85aefd40dbc60c4cefb10387fd28ee4
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-05 14:48:37 +0300

    removed empty line after doc, changed typeof checking to .toBeinstanceOf, moved expectation checks to new line, removed package-lock.json from .gitignore

--- src/arrayMethodPop.test.js
+++ src/arrayMethodPop.test.js
@@ -4,27 +4,34 @@ const applyCustomPop = require('./arrayMethodPop');
 
 applyCustomPop();
 
-test('`pop2` is added to [].__proto__', () => {
-  expect(typeof [].pop2).toBe('function');
+test('pop2 is added to [].__proto__', () => {
+  expect([].pop2)
+    .toBeInstanceOf(Function);
 });
 
 test('pop single item', () => {
   const source = [0, 1, 2, 3];
   const result = source.pop2();
-  expect(source).toEqual([0, 1, 2]);
-  expect(result).toBe(3);
+  expect(source)
+    .toEqual([0, 1, 2]);
+  expect(result)
+    .toBe(3);
 });
 
 test('pop from empty array', () => {
   const source = [];
   const result = source.pop2();
-  expect(source).toEqual([]);
-  expect(result).toBe(undefined);
+  expect(source)
+    .toEqual([]);
+  expect(result)
+    .toBe(undefined);
 });
 
 test('pop ignores arguments', () => {
   const source = [0, 1, 2, 3];
   const result = source.pop2(2);
-  expect(source).toEqual([0, 1, 2]);
-  expect(result).toBe(3);
+  expect(source)
+    .toEqual([0, 1, 2]);
+  expect(result)
+    .toBe(3);
 });
