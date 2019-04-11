e31e4da4462d94e412fbd0ad97efd723d3e4d5ad
Onisko <Onisko@MacBook.local>
2019-04-11 13:17:11 +0300

    sol

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,9 +5,14 @@
  *
  */
 function applyCustomPop() {
-  [].__proto__.pop2 = function() {
+  [].__proto__.pop2 = function(...theArgs) {
     // write code here
+    let a = +this.splice([this.length - 1], 1);
+    if (a > 0) {
+      return a;
+    } else {
+      return undefined;
+    }
   };
 }
-
 module.exports = applyCustomPop;
