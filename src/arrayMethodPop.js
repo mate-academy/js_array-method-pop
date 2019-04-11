d25cdabc58613d779228bf8b3cb144a5bc3c2336
Denis <krollik1000000@gmail.com>
2019-04-11 13:17:33 +0300

    'denisSkab'

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,8 +5,18 @@
  *
  */
 function applyCustomPop() {
-  [].__proto__.pop2 = function() {
-    // write code here
+  [].__proto__.pop2 = function(...args) {
+    let result = this.splice(this.length - 1, 1);
+    if (result.length === 0) {
+      return undefined;
+    }
+    if (result.length !== 0) {
+      let number = 0;
+      result.forEach(function(i) {
+        number += i;
+      });
+      return number;
+    }
   };
 }
 
