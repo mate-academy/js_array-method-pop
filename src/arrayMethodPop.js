71572ac7d163e2b16ca067cf470bffe56cf2c586
Vitalii Pshychenko <pshychenko94@gmail.com>
2019-04-11 23:06:06 +0300

    fixed1

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -8,8 +8,9 @@
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
     if (this.length > 0) {
+      const removedItem = this[this.length - 1];
       this.length -= 1;
-      return this.length;
+      return removedItem;
     }
   };
 }
