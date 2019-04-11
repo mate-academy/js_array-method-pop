8a3836d128bf190933347cf53f70e53dbc9fd125
Dima Siianytsia <ds081181@gmail.com>
2019-04-11 14:29:09 +0300

    solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -7,6 +7,12 @@
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
     // write code here
+    if (this.length === 0) {
+      return undefined;
+    }
+    let returnedElement = this[this.length - 1];
+    this.length--;
+    return returnedElement;
   };
 }
 
