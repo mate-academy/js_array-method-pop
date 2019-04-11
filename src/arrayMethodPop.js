f8da030a07a5e6bcc2f2874edc6743f3efc95882
Vitalii <ilovepinkpony1@gmail.com>
2019-04-11 16:58:57 +0300

    solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -8,7 +8,7 @@ function applyCustomPop() {
   [].__proto__.pop2 = function() {
     if (this.length === 0) return undefined;
     const deletedToken = this[this.length - 1];
-    this.length--;
+    this.length -= 1;
     return deletedToken;
   };
 }
