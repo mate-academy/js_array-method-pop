87223dcc2e03f9c8bd024ad74284b3aebfbd8931
VladimirTi <tytmes@mail.ru>
2019-04-11 17:19:49 +0300

    Update arrayMethodPop.js

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -7,8 +7,7 @@
 function applyCustomPop() {
   [].__proto__.pop2 = function(...args) {
     if (this.length > 0) {
-      this.length -= 1;
-      return this.length;
+      return this.length -= 1;
     }
   };
 }
