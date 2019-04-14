f67589fbfe892aa52e0afd42a0c4c570d7f94970
yojikyo <yojikyo@gmail.com>
2019-04-14 21:39:53 +0300

    'solution'

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -7,6 +7,17 @@
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
     // write code here
+    return (this === []) ? this : this.splice(this.length - 1, 1)[0];
+    // or this...
+    /*
+    if (this === []) {
+      return this
+    } else {
+      const element = this[this.length];
+      this.length -= 1;
+      return element;
+    }
+    */
   };
 }
 
