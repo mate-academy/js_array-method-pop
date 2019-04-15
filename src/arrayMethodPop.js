fde724eeabe507389bf52ceebdef56cd8991b9d4
gestap <gestap@gmail.com>
2019-04-15 11:05:36 +0300

    Solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -6,7 +6,13 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    // write code here
+    let a = this[this.length - 1];
+    if (this.length === 0) {
+      return undefined;
+    } else {
+      this.length--;
+    }
+    return a;
   };
 }
 
