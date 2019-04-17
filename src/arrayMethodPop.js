595c3cfdc74fc1f88316f874a3de09631dde80a1
Alexandr <alexandrmail32@gmail.com>
2019-04-17 18:16:46 +0300

    Now pop returns needed elem

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,12 +5,14 @@
  *
  */
 function applyCustomPop() {
+  let popElem;
   [].__proto__.pop2 = function() {
     if (!this.length) {
       return this[this.length];
     } else {
+      popElem = this[this.length - 1];
       this.length -= 1;
-      return this.length;
+      return popElem;
     }
   };
 }
