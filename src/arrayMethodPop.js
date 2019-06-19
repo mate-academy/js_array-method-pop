e6cf162e1a3c0e8b434377d6eabe47ae5eb0f2d0
bratyslav <beginner.altavista@gmail.com>
2019-06-19 17:35:36 +0300

    Solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,7 +5,12 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    // write code here
+    const removedValue = this[this.length - 1];
+    if (this.length === 0) {
+      return undefined;
+    }
+    this.length--;
+    return removedValue;
   };
 }
 
