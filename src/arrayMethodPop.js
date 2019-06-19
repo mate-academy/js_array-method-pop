560d94024cb580d19c05259591bfd23268583c1c
AnnaKuvarina <anya.kuvarina@gmail.com>
2019-06-19 14:52:58 +0300

    change return

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,11 +5,12 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
+    const deleteElem = this[this.length - 1];
     if (this.length < 1) {
       return undefined;
     } else {
       this.length--;
-      return this.length;
+      return deleteElem;
     }
   };
 }
