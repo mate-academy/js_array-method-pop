6497ecf105e6fbcbe8f189be06cf70ef28f36287
tearfulmerlin <t380937709275@gmail.com>
2019-06-20 19:33:52 +0300

    solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,8 +5,18 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    // write code here
+    if (this.length === 0) {
+      return undefined;
+    }
+
+    const popped = this[this.length - 1];
+    this.length--;
+    return popped;
   };
 }
-
+/* const n = [1, 2];
+applyCustomPop()
+console.log(n.pop2());
+console.log(n);
+ */
 module.exports = applyCustomPop;
