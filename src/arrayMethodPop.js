d14078d597770ee2d8a9ed36af11df7b3d2b3b78
GitHubForDmitry <dmitrykoka@gmail.com>
2019-06-21 18:37:43 +0300

    sol

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,7 +5,12 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    // write code here
+    if (this.length === 0) {
+      return undefined;
+    }
+    const delElement = this[this.length - 1];
+    this.length -= 1;
+    return delElement;
   };
 }
 
