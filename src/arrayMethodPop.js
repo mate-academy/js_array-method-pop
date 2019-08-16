563b94e8c1874d3ef307a9d1fc73e55c2c9ac7f5
Alina Kizilova <alinakzilova@gmail.com>
2019-08-16 17:09:14 +0300

    solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,7 +5,14 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    // write code here
+    if (!this.length) {
+      return undefined;
+    }
+
+    this.length -= 1;
+    const item = this[this.length - 1];
+
+    return item;
   };
 }
 
