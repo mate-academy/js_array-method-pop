c5aa6f1c671b9e669b726c4ce2f9f73a52890bd9
yevhen-sahach <antitop04@gmail.com>
2019-08-16 17:20:50 +0300

    first commit

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,7 +5,14 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    // write code here
+    if (this.length < 1) {
+      return undefined;
+    }
+
+    const lastElement = this[this.length - 1];
+    this.length -= 1;
+
+    return lastElement;
   };
 }
 
