e374dd4bea9ae178b8f173bb40dafaf82c1a4669
KseniiaMazan <ksusamazan@gmail.com>
2019-08-16 17:22:27 +0300

    fixed solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,7 +5,12 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    return this.length !== 0 ? (this.length = this.length - 1) : undefined;
+    const returnItem = (this.length !== 0 ? this[this.length - 1] : undefined);
+
+    if (this.length !== 0) {
+      this.length = this.length - 1;
+    }
+    return returnItem;
   };
 }
 
