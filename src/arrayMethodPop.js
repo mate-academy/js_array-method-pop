1e4235523709123f041091bf0dda2937e15dff45
Taisiia <taisiia.pomazan@gmail.com>
2019-08-18 20:37:57 +0300

    sol

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,7 +5,13 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    // write code here
+    if (this.length < 1) {
+      return undefined;
+    }
+    const lastElem = this[this.length - 1];
+    this.length = this.length - 1;
+
+    return lastElem;
   };
 }
 
