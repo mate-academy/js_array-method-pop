f7609a25eb92c1186360fc56eb830a3ae40381c2
Aleks <alex.andrutskii@gmail.com>
2019-08-18 22:54:37 +0300

    Fixed variable declaration

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,12 +5,10 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    let lastValueOfArray = null;
-
     if (this.length === 0) {
       return;
     }
-    lastValueOfArray = this[this.length - 1];
+    const lastValueOfArray = this[this.length - 1];
     this.length -= 1;
 
     return lastValueOfArray;
