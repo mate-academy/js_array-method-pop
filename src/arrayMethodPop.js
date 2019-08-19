71222ead28d0ca77ac70f795300a631d943bf27a
VladyslavSak <iblackvour@gmail.com>
2019-08-19 12:48:56 +0300

    Solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,7 +5,9 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    // write code here
+    const poppedElement = this[this.length - 1];
+    this.splice(this.length - 1);
+    return poppedElement;
   };
 }
 
