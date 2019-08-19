3e2644815a6d5aeac3283d081a8aa4de1b453757
Volodsher <volodsher@gmail.com>
2019-08-19 22:44:50 +0300

    my decision

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -1,11 +1,14 @@
 'use strict';
 
-/**
- * Implement method pop
- */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    // write code here
+    if (this.length === 0) {
+      return undefined;
+    }
+    const lastElement = this[this.length - 1];
+    this.length = this.length - 1;
+
+    return lastElement;
   };
 }
 
