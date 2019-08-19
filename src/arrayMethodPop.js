75cd9a7878137ba8d14e7f3d8b289eea4cd80f2b
Max_Arn <maximarnayt@gmail.com>
2019-08-19 14:07:48 +0300

    without methods

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,7 +5,14 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    return this.splice(-1)[0];
+    if (this.length === 0) {
+      return undefined;
+    }
+
+    const lastItem = this[this.length - 1];
+    this.length -= 1;
+
+    return lastItem;
   };
 }
 
