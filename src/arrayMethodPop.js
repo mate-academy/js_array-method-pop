313359810ffeb1ca8604d4ce36c200c67eb5ebe3
ChernijAnna <chernij.anna@gmail.com>
2019-08-20 19:44:24 +0300

    delite variable

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,11 +5,10 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    const length = this.length;
-    if (length === 0) {
+    if (this.length === 0) {
       return undefined;
     }
-    const element = this[length - 1];
+    const element = this[this.length - 1];
     this.length -= 1;
     return element;
   };
