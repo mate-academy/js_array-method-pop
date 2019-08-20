f10d08c5bc20e1b752fd37903359d1557a07e84f
VladyslavSak <iblackvour@gmail.com>
2019-08-20 10:56:54 +0300

    fix, without methods to array

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,8 +5,13 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
+    if (this.length === 0) {
+      return undefined;
+    }
+
     const poppedElement = this[this.length - 1];
-    this.splice(this.length - 1);
+
+    this.length -= 1;
     return poppedElement;
   };
 }
