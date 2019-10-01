30b2b2c991a665520950d1e6f6b9712573101eb1
Andrii <farikpite@gmail.com>
2019-10-01 19:28:45 +0300

    Solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,11 +5,10 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    if (this.length === 0) {
-      return undefined;
-    }
     const item = this[this.length - 1];
-    this.length--;
+    if (this.length > 0) {
+      this.length--;
+    }
     return item;
   };
 }
