4c31c033a94f6bc5cffb13c028db2c818bd90a95
ITStan <s.stanislavskaya@gmail.com>
2019-10-01 23:32:55 +0300

    Solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,7 +5,16 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    // write code here
+    if (this.length === 0) { // Проверяем массив на пустоту
+      return undefined;
+    }
+
+    // this[this.length - 1] - последний элемент массива
+    const deletedElement = this[this.length - 1];
+    // Уменьшаем длину массива на 1 (аналог удаления последнего элемента)
+    this.length--;
+
+    return deletedElement;
   };
 }
 
