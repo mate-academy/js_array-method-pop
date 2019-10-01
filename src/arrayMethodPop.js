19572fb595856172223085779faaf5890b68cb07
Viktor Lukovets <nuerio63@gmail.com>
2019-10-01 18:00:20 +0300

    no arr methods

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,7 +5,13 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    return this.splice(-1, 1)[0];
+    const arrLength = this.length;
+    if (arrLength === 0) {
+      return undefined;
+    }
+    const lastItem = this[arrLength - 1];
+    this.length -= 1;
+    return lastItem;
   };
 }
 
