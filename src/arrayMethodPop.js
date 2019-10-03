bc732e554ac4b13a8152981585134d4f1e74c9e6
VitaliyStarodub <starodub.vital@gmail.com>
2019-10-04 00:38:53 +0300

    'solution_vitalii'

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -6,6 +6,14 @@
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
     // write code here
+    if (this.length !== 0) {
+      const lastElem = this[this.length - 1];
+      delete this[this.length - 1];
+      this.length -= 1;
+      return lastElem;
+    } else {
+      return undefined;
+    }
   };
 }
 
