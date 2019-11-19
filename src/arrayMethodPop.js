517c523224560b956fd11669acf6191b8511fee5
Alexandra Boyko <Alexboyko04@gmail.com>
2019-11-19 19:07:41 +0200

    Solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,7 +5,13 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    // write code here
+    if (!this.length) {
+      return undefined;
+    }
+
+    const lastValue = this[this.length - 1];
+    this.length = this.length - 1;
+    return lastValue;
   };
 }
 
