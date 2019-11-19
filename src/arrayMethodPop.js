a7e4604f787d961fd9a5f178b85a2a28fb264f02
Oles Marola <oles_pw@ukr.net>
2019-11-19 17:14:13 +0200

    pop

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,14 +5,12 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    const lastElement = this[this.length - 1];
     if (this.length > 1) {
+      const lastElement = this[this.length - 1];
       this.length = this.length - 1;
       return lastElement;
-    } else {
-      delete this;
-      return undefined;
     }
+    return undefined;
   };
 }
 
