ebd748716060c6056705771144a9a662cd092cb9
helmax-y <max_rad@ukr.net>
2019-11-19 23:57:07 +0200

    Solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,7 +5,15 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    // write code here
+    if (this.length === 0) {
+      return undefined;
+    }
+
+    const returnedValue = this[this.length - 1];
+    delete this[this.length - 1];
+    this.length--;
+
+    return returnedValue;
   };
 }
 
