3b4d12e04db64ed949d5960f209ebfdb3d8ab09e
boom40 <kickgames675@gmail.com>
2019-11-20 16:20:46 +0200

    solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,7 +5,11 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    // write code here
+    if (this.length === 0) {
+      return undefined;
+    }
+    const deleteItem = this.splice(-1);
+    return +deleteItem;
   };
 }
 
