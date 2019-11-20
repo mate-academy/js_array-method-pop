db8fef83ee8a14b40cd4f045ba2362617523fcff
natya2203 <nastya_2203@ukr.net>
2019-11-20 12:31:13 +0200

    solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,7 +5,11 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    // write code here
+    if (this.length > 0) {
+      const item = this[this.length - 1];
+      this.length = this.length - 1;
+      return item;
+    }
   };
 }
 
