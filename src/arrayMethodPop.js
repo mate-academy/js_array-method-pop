952c63407d3e848faf318569f92071c009773362
LutsenkoAnV <48694313+LutsenkoAnV@users.noreply.github.com>
2020-01-25 17:43:48 +0200

    solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,8 +5,19 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    // write code here
+    const deleted = this[this.length - 1];
+
+    this.splice(this.length - 1, 1);
+
+    return deleted;
   };
 }
+
+// applyCustomPop();
+
+// const a = [1, 2, 3, 4];
+
+// console.log(a.pop2());
+// console.log(a);
 
 module.exports = applyCustomPop;
