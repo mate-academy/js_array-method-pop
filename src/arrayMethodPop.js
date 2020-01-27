9d181b5d4a3d3012dae5af2c2414fbc5fb75b2ad
Stanislav Mochyr <delenjer@gmail.com>
2020-01-27 18:46:34 +0200

    change

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,13 +5,13 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    if (this.length === 0) {
+    if (!this.length) {
       return undefined;
     }
 
     const lastElement = this[this.length - 1];
 
-    this.length = this.length - 1;
+    this.length--;
 
     return lastElement;
   };
