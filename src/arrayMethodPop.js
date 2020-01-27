262be7aafa4dd438e54184c7f6233fe81cd212b1
Valeriy-Sorochynskyi <haribol162@gmail.com>
2020-01-27 18:37:37 +0200

    if on top

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,11 +5,13 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
+    if (!this.length) {
+      return undefined;
+    }
+
     const lastItem = this[this.length - 1];
 
-    if (this.length !== 0) {
-      this.length = this.length - 1;
-    }
+    this.length -= 1;
 
     return lastItem;
   };
