97e6c38e75c72d611c8227f2ec85fced1ac9d642
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-01-27 17:51:33 +0200

    fix this.length check to 0 & this.length - 1

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,13 +5,13 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    if (this.length === 0) {
+    if (!this.length) {
       return undefined;
     }
 
     const poppedElement = this[this.length - 1];
 
-    this.length = this.length - 1;
+    this.length--;
 
     return poppedElement;
   };
