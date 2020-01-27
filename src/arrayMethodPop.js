bb5a2bb75963a73d7d38e26b79674a44247e7daa
Viacheslav <slavic.lavrenyuk@gmail.com>
2020-01-27 14:58:38 +0200

    Without Else

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -7,13 +7,13 @@ function applyCustomPop() {
   [].__proto__.pop2 = function() {
     if (this.length === 0) {
       return undefined;
-    } else {
-      const popElement = this[this.length - 1];
-
-      this.length -= 1;
-
-      return popElement;
     }
+
+    const popElement = this[this.length - 1];
+
+    this.length -= 1;
+
+    return popElement;
   };
 }
 
