194a1ce3bf2944ff8434b1a3542af33061faebf7
oksanaDaniluk <oksana.daniluk@ukr.net>
2020-01-27 18:09:49 +0200

    solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -4,8 +4,17 @@
  * Implement method pop
  */
 function applyCustomPop() {
-  [].__proto__.pop2 = function() {
-    // write code here
+  // eslint-disable-next-line no-extend-native
+  Array.prototype.pop2 = function() {
+    if (!this.length) {
+      return undefined;
+    }
+
+    const lastElement = this[this.length - 1];
+
+    this.length -= 1;
+
+    return lastElement;
   };
 }
 
