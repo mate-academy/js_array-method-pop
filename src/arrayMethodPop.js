813895927855682f47b1af2af726992f0de85eba
dima <tynindima@gmail.com>
2020-01-28 12:24:07 +0200

    Recall poped to popped

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,13 +5,13 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    const popedElement = this[this.length - 1];
+    const poppedElement = this[this.length - 1];
 
-    if (popedElement !== undefined) {
+    if (poppedElement !== undefined) {
       this.length -= 1;
     }
 
-    return popedElement;
+    return poppedElement;
   };
 }
 
