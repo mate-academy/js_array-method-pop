9f43582f16d0949604a275cb968d553025097724
Olexander <kravchenko.sania@gmail.com>
2020-01-28 17:42:13 +0200

    attempt 3

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,11 +5,13 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    const lastElement = this[this.length - 1];
-
-    if (lastElement !== undefined) {
-      this.length--;
+    if (!this.length) {
+      return undefined;
     }
+
+    const lastElement = this[(this.length) - 1];
+
+    this.length--;
 
     return lastElement;
   };
