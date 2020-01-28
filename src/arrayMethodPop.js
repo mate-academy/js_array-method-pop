8e8213456dcdcfec36ea4c41af0f90d570bc38a0
Viktoria <valencia88vika@gmail.com>
2020-01-28 08:43:53 +0200

    change const

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,14 +5,14 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    const elem = this[this.length - 1];
+    const lastElement = this[this.length - 1];
 
     if (this.length === 0) {
       return undefined;
     }
     this.length = this.length - 1;
 
-    return elem;
+    return lastElement;
   };
 }
 
