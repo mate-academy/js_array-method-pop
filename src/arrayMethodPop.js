39bf82f086b684a44bfc212eafc9c195cc33c6d1
Al145 <47367383+Al145@users.noreply.github.com>
2020-01-28 22:29:03 +0200

    solut

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -6,6 +6,17 @@
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
     // write code here
+    const arrLength = this.length;
+
+    if (arrLength === 0) {
+      return undefined;
+    }
+
+    const result = this[arrLength - 1];
+
+    this.length = arrLength - 1;
+
+    return result;
   };
 }
 
