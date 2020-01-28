f45081df8c8d0783c8076b0eb5bfa9fe6f14ee14
Yuliya312 <yu312liya@gmail.com>
2020-01-28 10:08:16 +0200

    fix undefind

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -8,9 +8,11 @@ function applyCustomPop() {
     if (this.length) {
       const item = this[this.length - 1];
 
-      this.length = this.length - 1;
+      this.length -= 1;
 
       return item;
+    } else {
+      return undefined;
     }
   };
 }
