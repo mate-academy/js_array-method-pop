65c5b40e846bf239aff45b7b976ffa48281ad8f2
crash2bash <sachagurov@gmail.com>
2020-03-30 18:56:42 +0300

    fix

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -7,11 +7,9 @@ function applyCustomPop() {
   [].__proto__.pop2 = function() {
     let popItem;
 
-    for (let i = 0; i < this.length; i++) {
-      if (i === this.length - 1) {
-        popItem = this[i];
-        this.length = this.length - 1;
-      }
+    if (this.length > 0) {
+      popItem = this[this.length - 1];
+      this.length = this.length - 1;
     }
 
     return popItem;
