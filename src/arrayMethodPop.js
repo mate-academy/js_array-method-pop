745ac0c64a5b67c37a4e6e6247777f438bfae839
kate <kate@test.com>
2020-04-01 20:28:54 +0300

    solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -6,7 +6,17 @@
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
     // write code here
+    const arr = this;
+
+    if (arr.length === 0) {
+      return;
+    }
+
+    const last = JSON.parse(JSON.stringify(arr[arr.length - 1]));
+
+    arr.length--;
+
+    return last;
   };
 }
-
 module.exports = applyCustomPop;
