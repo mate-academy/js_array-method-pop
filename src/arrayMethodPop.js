489a8f0afe94227ab23032445f25a88eb43fc366
Andrey Faychak <faychak333@gmail.com>
2020-04-01 18:23:32 +0300

    'solution'

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -4,8 +4,17 @@
  * Implement method pop
  */
 function applyCustomPop() {
-  [].__proto__.pop2 = function() {
+  [].__proto__.pop2 = function(n) {
     // write code here
+    let popElement;
+
+    if (this.length === 0) {
+      return undefined;
+    } else {
+      popElement = this.splice(this.length - 1, 1);
+    }
+
+    return popElement[0];
   };
 }
 
