1d096e3de95a68661120c333a44a5322dd95e57a
AlenaLoik <CrazyBeeLviv@gmail.com>
2020-04-01 18:55:38 +0300

    solution 2

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,9 +5,13 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    const res = this.reverse().shift();
+    const res = this[this.length - 1];
 
-    this.reverse();
+    if (this.length > 0) {
+      this.length = this.length - 1;
+    } else {
+      this.length = this.length = 0;
+    }
 
     return res;
   };
