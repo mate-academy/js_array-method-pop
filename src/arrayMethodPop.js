b0a8e87acfc9e5ca0800e0846989bd3768cd4f02
Vitalii Korol <vitaliikorol@github.com>
2020-04-01 16:53:46 +0300

    solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,7 +5,15 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    // write code here
+    const object = this[this.length - 1];
+
+    if (this.length === 0) {
+      return undefined;
+    } else {
+      this.length = this.length - 1;
+
+      return object;
+    }
   };
 }
 
