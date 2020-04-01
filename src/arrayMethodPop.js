e4300f94ea75222a3d3518c6ee80d65f07acac3f
AnnieMarkina <anniemurkina@gmail.com>
2020-04-01 20:18:26 +0300

    solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,7 +5,15 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    // write code here
+    if (this.length === 0) {
+      return undefined;
+    }
+
+    const removedEl = this[this.length - 1];
+
+    this.length -= 1;
+
+    return removedEl;
   };
 }
 
