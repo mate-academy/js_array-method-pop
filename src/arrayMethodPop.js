20dcdcce31e81fbb698de2249dbbee7b00fc8f93
Alex Machula <alexmachulamail@gmail.com>
2020-04-01 22:38:04 +0300

    solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,7 +5,17 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    // write code here
+    const item = this.length - 1;
+    const lastElem = this[item];
+
+    if (this.length !== 0) {
+      delete this[item];
+      this.length = item;
+
+      return lastElem;
+    } else {
+      return undefined;
+    }
   };
 }
 
