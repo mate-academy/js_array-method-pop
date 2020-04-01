9be35ae659318bd130625de2b5d57b8e823a05bd
Yarik1993 <makarov1603@icloud.com>
2020-04-01 18:53:44 +0300

    'solution'

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -6,6 +6,15 @@
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
     // write code here
+    const answer = this[this.length - 1];
+
+    if (this.length === 0) {
+      return undefined;
+    } else {
+      this.length = this.length - 1;
+    }
+
+    return answer;
   };
 }
 
