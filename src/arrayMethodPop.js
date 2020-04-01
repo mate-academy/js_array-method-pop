034a45708aba3f253d6d2f3072b5a702ce122fd1
BorisovecViktor <viktorborisovets@gmail.com>
2020-04-01 18:04:26 +0300

    solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,7 +5,17 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    // write code here
+    const item = this.length - 1;
+    const pop = this[item];
+
+    if (this.length !== 0) {
+      delete this[item];
+      this.length = item;
+
+      return pop;
+    } else {
+      return undefined;
+    }
   };
 }
 
