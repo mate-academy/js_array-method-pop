cb32a7c9812dfa3f1bf93ea4d2267f3c5032012e
Roma <roman.mykhalchuk@yahoo.com>
2020-04-01 20:58:20 +0300

    solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,7 +5,16 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    // write code here
+    let lastArrElem;
+
+    if (this.length === 0) {
+      return undefined;
+    } else {
+      lastArrElem = this[this.length - 1];
+      this.length = this.length - 1;
+    }
+
+    return lastArrElem;
   };
 }
 
