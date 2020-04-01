58a932f868bf29b81a8ed944e3c904f851448118
sanyanemesh <sanya.nemesh@gmail.comgit config --global user.name sanyanemeshgit config --global user.email sanya.nemesh@gmail.com>
2020-04-01 20:21:03 +0300

    Solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,7 +5,13 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    // write code here
+    const result = this[this.length - 1];
+
+    if (this.length === 0) {
+      this.length -= 1;
+    }
+
+    return result;
   };
 }
 
