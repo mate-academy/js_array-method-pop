6ac3a37df5e394715214a06ef516724d26b210b7
BuildTools <unconfigured@null.spigotmc.org>
2020-04-04 19:22:13 +0300

    my solution

--- src/arrayMethodPop.test.js
+++ src/arrayMethodPop.test.js
@@ -17,26 +17,36 @@ test(`pop2 doesn't call default pop`, () => {
 test('pop single item', () => {
   const source = [5, 6, 7, 8];
   const result = source.pop2();
+
   expect(source)
+
     .toEqual([5, 6, 7]);
+
   expect(result)
     .toBe(8);
 });
 
 test('pop from empty array', () => {
   const source = [];
+
   const result = source.pop2();
+
   expect(source)
     .toEqual([]);
+
   expect(result)
     .toBe(undefined);
 });
 
 test('pop ignores arguments', () => {
   const source = [7, 6, 5, 1];
+
   const result = source.pop2(2);
+
   expect(source)
+
     .toEqual([7, 6, 5]);
+
   expect(result)
     .toBe(1);
 });
