7e3fc0f914908478b2347f37b38d5d8196836ad3
Andrii Dmytriiev <andriidmytriev@gmail.com>
2020-05-05 11:36:18 +0300

    solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -6,6 +6,13 @@
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
     // write code here
+    if (this.length !== 0) {
+      const element = this[this.length - 1];
+
+      this.length -= 1;
+
+      return element;
+    }
   };
 }
 
