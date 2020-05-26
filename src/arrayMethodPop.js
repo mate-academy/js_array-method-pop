5bad72d1f869c8cd4737c1fa354fa0f8925a2722
solaryasha <solaryasha@gmail.com>
2020-05-26 15:14:46 +0300

    My solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -6,6 +6,17 @@
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
     // write code here
+    if (!this.length) {
+      return undefined;
+    }
+
+    const indexOfLastElement = this.length - 1;
+
+    const lastElement = this[indexOfLastElement];
+
+    this.length = indexOfLastElement;
+
+    return lastElement;
   };
 }
 
