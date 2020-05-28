bb97ffa4d79a8d715ded221dd5baeec934b0f903
German <germandolia@gmail.com>
2020-05-28 22:47:19 +0300

    solution

--- src/arrayMethodPop.js
+++ src/arrayMethodPop.js
@@ -5,7 +5,13 @@
  */
 function applyCustomPop() {
   [].__proto__.pop2 = function() {
-    // write code here
+    const poppedElement = this[this.length - 1];
+
+    if (this.length > 0) {
+      this.length--;
+    }
+
+    return poppedElement;
   };
 }
 
